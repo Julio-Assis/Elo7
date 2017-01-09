09b2bffe3e3aab57717886a4140669931ef98b21
Julio-Assis <juliosevarollia@gmail.com>
2017-01-09 12:15:00 -0200

    Add files via upload

---
+++ src/RunProject.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+//main antigo que foi utilizado para testes
+public class RunProject {
+	/*public static void main(String []args){
+		Upland upland;
+		Scanner sc;
+		try {
+			sc = new Scanner(new File("segundoTeste.txt"));
+			int width = sc.nextInt() + 1;
+			int height = sc.nextInt() + 1;
+			upland = new Upland(width, height);
+			while(sc.hasNext()){
+				int posX = sc.nextInt();
+				int posY = sc.nextInt();
+				String dir = sc.next();
+				String comands = sc.next();
+				upland.addOperator(new Operator(posX,posY,dir,comands,upland));				
+			}
+			upland.processOperatorComands();
+			upland.printStates();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	*/
+}
